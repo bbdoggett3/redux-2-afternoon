40d999dca254bfe8e9a8279a8884aab89cc9243e
bbdoggett3 <bbdoggett3@gmail.com>
2020-06-01 16:10:02 -0600

    add step6

--- src/components/Budget/Budget.js
+++ src/components/Budget/Budget.js
@@ -7,34 +7,34 @@ import DisplayPurchases from './../shared/DisplayPurchases';
 import Loading from './../shared/Loading/Loading';
 import Nav from './../shared/Nav';
 import './Budget.css';
-import {connect} from 'react-redux';
-import { requestUserData} from '../../ducks/userReducer';
- 
+import { connect } from 'react-redux'
+import { requestUserData } from './../../ducks/userReducer'
+import { requestBudgetData } from './../../ducks/budgetReducer'
 
 
 class Budget extends Component {
-
   componentDidMount() {
     this.props.requestUserData();
+    this.props.requestBudgetData();
   }
 
   render() {
-    const {loading} = this.props.budget;
-    const {firstName, lastName} = this.props.user;
-    console.log(this.props.user)
+  // DESTRUCTURING VALUES HERE IS OPTIONAL BUT RECOMMENDED FOR CLEANER JSX
+    const { loading, purchases, budgetLimit } = this.props.budget;
+    const { firstName, lastName } = this.props.user;
     return (
       <Background>
         {loading ? <Loading /> : null}
         <div className='budget-container'>
-          <Nav firstName = {firstName} lastName = {lastName} />
+          <Nav firstName={firstName} lastName={lastName} />
           <div className='content-container'>
             <div className="purchases-container">
               <AddPurchase />
-              <DisplayPurchases />
+              <DisplayPurchases purchases={purchases} />
             </div>
             <div className='chart-container'>
-              <Chart1 />
-              <Chart2 />
+              <Chart1 purchases={purchases} budgetLimit={budgetLimit} />
+              <Chart2 purchases={purchases} />
             </div>
           </div>
         </div>
@@ -50,4 +50,4 @@ function mapStateToProps(state) {
   }
 }
 
-export default connect(mapStateToProps, {requestUserData})(Budget);
+export default connect(mapStateToProps, { requestUserData, requestBudgetData })(Budget);
